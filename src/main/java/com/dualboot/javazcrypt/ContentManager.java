7136eea07f05a39bb53344459c08cc95bf073880
Daniel Oliveira <oliveira.d.c@outlook.com>
2024-01-31 13:49:02 -0300

    fix dependency of ContentManager on variable in FileEncryptionUtility

--- src/main/java/com/dualboot/javazcrypt/ContentManager.java
+++ src/main/java/com/dualboot/javazcrypt/ContentManager.java
@@ -70,20 +70,6 @@ public class ContentManager {
     }
 
     public static void writeBytesToFile(String outputFile, byte[] outputBytes) throws IOException {
-        
-        Path outputFilePath = Paths.get(outputFile);
-        if (Files.exists(outputFilePath) && !Main.inPlace) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("File already exists. Would you like to overwrite it? (y/n)");
-            String overwrite = scanner.nextLine();
-            overwrite.toLowerCase();
-            if (!overwrite.equals("y") && !overwrite.equals("yes")) {
-                System.out.println("Operation aborted.");
-                System.exit(1);
-            }
-
-        }
-        
         try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
             outputStream.write(outputBytes);
         }
