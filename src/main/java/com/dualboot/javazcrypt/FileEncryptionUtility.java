d6bd9613348a87d8e7643fee6a17f3679b97134d
Daniel Oliveira <oliveira.d.c@outlook.com>
2024-01-31 13:38:56 -0300

    rename files - FileEncryptionUtility (former Main.java) is not actively being used as it served to make a sketch of the current Main.java

---
+++ src/main/java/com/dualboot/javazcrypt/FileEncryptionUtility.java
@@ -0,0 +1,98 @@
+package com.dualboot.javazcrypt;
+
+import java.io.Console;
+
+public class FileEncryptionUtility {
+
+    public static boolean inPlace = false;
+    
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.err.println("No arguments were provided.\nDisplaying help instead:"); return;
+        }
+
+        if (args.length == 1) {
+            System.err.println("Not enough arguments were provided.\nDisplaying help instead:"); return;
+        }
+        
+        String keyFile = null;
+        String operation = null;
+        String inputFile = null;
+        String outputFile = null;
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-e":
+                case "--encrypt":
+                    operation = "encrypt";
+                    break;
+                case "-d":
+                case "--decrypt":
+                    operation = "decrypt";
+                    break;
+                case "-k":
+                    if (i < args.length - 1) {
+                        keyFile = args[++i]; // ++i - Move to the next argument - keyfile path won't serve in switch case anyway
+                    } else {
+                        System.out.println("Missing key file.");
+                        System.exit(1);
+                    }
+                    break;
+                case "-o":
+                case "--output":
+                    if (i < args.length -1) {
+                        outputFile = args[++i];
+                    } else {
+                        System.err.println("Missing output file.");
+                        System.exit(1);
+                    }
+                    break;
+                case "-i":
+                case "--in-place":
+                    inPlace = true;
+                    break;
+                default:
+                    inputFile = args[i];
+            }
+        }
+
+        Console console = System.console();
+        char[] passwordChars = console.readPassword("Enter your password: ");
+        String password = new String(passwordChars);
+
+        if (operation.equals("encrypt")) {
+
+            // double check password
+            char[] passwordChars2 = console.readPassword("Confirm your password: ");
+            String password2 = new String(passwordChars);
+            if (!password2.equals(password)) {
+                System.err.println("Passwords don't match. Aborting operation.");
+                System.exit(1);
+            }
+
+            try {
+                byte[] encryptedBytes = CryptOps.encryptFile(inputFile, password, keyFile);
+                if (inPlace == true) ContentManager.writeBytesToFile(inputFile,encryptedBytes);
+                if (outputFile != null) ContentManager.writeBytesToFile(outputFile,encryptedBytes);
+            } catch (Exception e) {
+                System.out.println("Error while encrypting file");
+            }
+
+        } else if (operation.equals("decrypt")) {
+            
+            try {
+                byte[] decryptedBytes = CryptOps.decryptFile(inputFile, password,keyFile);
+                if (inPlace == true) ContentManager.writeBytesToFile(inputFile,decryptedBytes);
+                if (outputFile != null) ContentManager.writeBytesToFile(outputFile,decryptedBytes);
+                if (inPlace == false && outputFile == null) ContentManager.printBytes(decryptedBytes);
+            } catch (Exception e) {
+                System.out.println("Error while decrypting file");
+                e.printStackTrace();
+            }
+            
+        } else {
+            System.err.println("Operation not recognized.");
+        }
+    }
+}
--- src/main/java/com/dualboot/javazcrypt/Main.java
+++ src/main/java/com/dualboot/javazcrypt/Main.java
@@ -1,36 +1,44 @@
 package com.dualboot.javazcrypt;
 
 import java.io.Console;
+import java.util.Scanner;
+
+// file checking
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// dont know which ones are needed
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 
 public class Main {
 
-    public static boolean inPlace = false;
-    
     public static void main(String[] args) {
 
         if (args.length == 0) {
             System.err.println("No arguments were provided.\nDisplaying help instead:"); return;
         }
 
-        if (args.length == 1) {
-            System.err.println("Not enough arguments were provided.\nDisplaying help instead:"); return;
-        }
-        
         String keyFile = null;
-        String operation = null;
+        String operation = "open";
         String inputFile = null;
-        String outputFile = null;
 
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
-                case "-e":
-                case "--encrypt":
-                    operation = "encrypt";
-                    break;
-                case "-d":
-                case "--decrypt":
-                    operation = "decrypt";
-                    break;
                 case "-k":
                     if (i < args.length - 1) {
                         keyFile = args[++i]; // ++i - Move to the next argument - keyfile path won't serve in switch case anyway
@@ -39,60 +47,144 @@ public class Main {
                         System.exit(1);
                     }
                     break;
-                case "-o":
-                case "--output":
+                case "-c":
+                case "--create":
+                    operation = "create";
                     if (i < args.length -1) {
-                        outputFile = args[++i];
+                        inputFile = args[++i];
                     } else {
-                        System.err.println("Missing output file.");
-                        System.exit(1);
+                        System.err.println("Missing database name.");
                     }
-                    break;
-                case "-i":
-                case "--in-place":
-                    inPlace = true;
-                    break;
                 default:
                     inputFile = args[i];
             }
         }
 
+        if (inputFile == null) {
+            System.err.println("No database specified.");
+            System.exit(1);
+        } else if (!operation.equals("create")) {
+            Path inputFilePath = Paths.get(inputFile);
+            if (!Files.exists(inputFilePath) || !Files.isRegularFile(inputFilePath)) {
+                System.err.printf("Could not find database %s%n Exiting.%n",inputFile);
+                System.exit(1);
+            }
+        }
+
+        if (keyFile != null) {
+            Path keyFilePath = Paths.get(keyFile);
+            if (!Files.exists(keyFilePath) || !Files.isRegularFile(keyFilePath)) {
+                System.err.printf("Could not find key file %s%nExiting.%n",keyFile);
+                System.exit(1);
+            }
+        }
+
         Console console = System.console();
-        char[] passwordChars = console.readPassword("Enter your password: ");
-        String password = new String(passwordChars);
+        char[] passwordChars = null;
+        String password = null;
+        String password2 = null;
 
-        if (operation.equals("encrypt")) {
-
-            // double check password
-            char[] passwordChars2 = console.readPassword("Confirm your password: ");
-            String password2 = new String(passwordChars);
-            if (!password2.equals(password)) {
-                System.err.println("Passwords don't match. Aborting operation.");
+        if (operation.equals("create")) {
+            try {
+                DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                Document passwordDatabase = documentBuilder.newDocument();
+                // PasswordDatabase passwordDataBase = new PasswordDatabase(documentBuilder.newDocument());
+                Element rootElement = passwordDatabase.createElement("dir");
+                rootElement.setAttribute("name","rootFolder");
+                passwordDatabase.appendChild(rootElement);
+                // Element element = passwordDatabase.createElement("personal");
+                // element.appendChild(passwordDatabase.createTextNode("Value"));
+                // rootElement.
+                // rootElement.appendChild(element);
+                Element newFolder = ContentManager.createFolder(passwordDatabase,rootElement,"personal");
+                Element newEntry = ContentManager.createEntry(passwordDatabase,newFolder,"outlook");
+                do {
+                    passwordChars = console.readPassword("Enter a password to encrypt the file: ");
+                    password = new String(passwordChars);
+                    passwordChars = console.readPassword("Confirm your password: ");
+                    password2 = new String(passwordChars);
+                    if (!password2.equals(password)) System.out.println("Passwords do not match. Try again.");
+                } while (!password2.equals(password));
+                byte[] decryptedBytes = convertXMLDocumentToByteArray(passwordDatabase);
+                ContentManager.writeBytesToFile(inputFile+"decrypted",decryptedBytes);
+                byte[] encryptedBytes = CryptOps.encryptBytes(decryptedBytes,password,keyFile);
+                ContentManager.writeBytesToFile(inputFile,encryptedBytes);
+            } catch(Exception e) {
+                System.err.println("Error creating database.");
+                e.printStackTrace();
                 System.exit(1);
             }
 
-            try {
-                byte[] encryptedBytes = CryptOps.encryptFile(inputFile, password, keyFile);
-                if (inPlace == true) ContentManager.writeBytesToFile(inputFile,encryptedBytes);
-                if (outputFile != null) ContentManager.writeBytesToFile(outputFile,encryptedBytes);
-            } catch (Exception e) {
-                System.out.println("Error while encrypting file");
-            }
-
-        } else if (operation.equals("decrypt")) {
-            
-            try {
-                byte[] decryptedBytes = CryptOps.decryptFile(inputFile, password,keyFile);
-                if (inPlace == true) ContentManager.writeBytesToFile(inputFile,decryptedBytes);
-                if (outputFile != null) ContentManager.writeBytesToFile(outputFile,decryptedBytes);
-                if (inPlace == false && outputFile == null) ContentManager.printBytes(decryptedBytes);
-            } catch (Exception e) {
-                System.out.println("Error while decrypting file");
-                e.printStackTrace();
-            }
-            
         } else {
-            System.err.println("Operation not recognized.");
+            passwordChars = console.readPassword("Enter your password: ");
+            password = new String(passwordChars);
         }
+
+        try {
+            byte[] decryptedBytes = CryptOps.decryptFile(inputFile, password,keyFile);
+            Document passwordDatabase = convertByteArrayToXMLDocument(decryptedBytes);
+            System.out.println("Database opened successfuly.");
+            // ContentManager.printBytes(decryptedBytes);
+        } catch (Exception e) {
+            System.out.println("Error opening database");
+        }
+        // menu here
+        String input;
+        do {
+            System.out.println("ls -l ");
+            System.out.println("-");
+            // List folders and items here
+            System.out.println("-");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the chosen option: ");
+            input = scanner.nextLine();
+
+        } while (!input.equals("e"));
+
+        // if (operation.equals("encrypt")) {
+
+        //     // double check password
+        //     char[] passwordChars2 = console.readPassword("Confirm your password: ");
+        //     String password2 = new String(passwordChars);
+        //     if (!password2.equals(password)) {
+        //         System.err.println("Passwords don't match. Aborting operation.");
+        //         System.exit(1);
+        //     }
+
+        //     try {
+        //         byte[] encryptedBytes = CryptOps.encryptFile(inputFile, password, keyFile);
+        //         if (inPlace == true) ContentManager.writeBytesToFile(inputFile,encryptedBytes);
+        //         if (outputFile != null) ContentManager.writeBytesToFile(outputFile,encryptedBytes);
+        //     } catch (Exception e) {
+        //         System.out.println("Error while encrypting file");
+        //     }
+
+        // } else if (operation.equals("decrypt")) {
+
+
+
+        // } else {
+        //     System.err.println("Operation not recognized.");
+        // }
+    }
+
+    private static byte[] convertXMLDocumentToByteArray(Document xmlDocument) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // Transform the XML document into a byte array
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        transformer.transform(new DOMSource(xmlDocument), new StreamResult(outputStream));
+
+        return outputStream.toByteArray();
+    }
+
+    private static Document convertByteArrayToXMLDocument(byte[] byteArray) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArray);
+
+        return builder.parse(inputStream);
     }
 }
