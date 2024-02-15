cd076595bc01008be0d78699cc7d6baf66840359
Daniel <oliveira.d.c@outlook.com>
2024-02-14 22:46:18 -0300

    add alert for deprecations on compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
+    options.compilerArgs.add("-Xlint:deprecation")
 }
 
 tasks.withType<Javadoc>() {
